224c1c9bd1c3622845e1e788821060dfb93f87b4
Marcus Manuel Feliciano <marcus@feliciano.eu>
2022-09-13 18:48:00 +0200

    Initial commit

---
+++ Authentication Server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const socket = require('socket.io')
+const fs = require('fs');
+const app = express();
+var PORT = process.env.PORT || 3000
+const server = app.listen(PORT)
+
+
+app.use(express.static('public'));
+console.log("Server is running")
+const io = socket(server)
+
+
+io.on('connection', (socket) => {
+    console.log("Socket connection: " + socket.id)
+
+    //Følgende linje burde hente IP-addresse, men aner ikke om den gjør noe som helst
+    console.log(socket.request.connection.remoteAddress)
+    
+})
